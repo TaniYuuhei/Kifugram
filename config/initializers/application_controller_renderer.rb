cd5e90065f22e1c8739638da53adc75d781197d7
TaniYuuhei <ty2630151047@gmail.com>
2021-09-20 00:05:20 +0900

    kifugramのコミット

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '02cd461e6ae7a1a854282561e5e3325c4fa3a9337fa1680f02c2833592e68ea5ec552640c74c6b2f834fa53092cc232b5ec6c941b91cb28a377da382760c0863'
